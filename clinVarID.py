fcff7ba585dfed1c6fd7ae9887f09557d387eb3d
maryastrinaki <32954315+maryastrinaki@users.noreply.github.com>
2018-10-07 19:03:32 +0300

    Update clinVarID.py

--- clinVarID.py
+++ clinVarID.py
@@ -1,4 +1,5 @@
 import xml.sax
+
 import gzip
 import xml.sax.saxutils as saxutils
 import json
@@ -13,6 +14,8 @@ class ClinVarHandler( xml.sax.ContentHandler):
 		self.data_line=[]
 		self.Title=[]
 		self.childlist_title=""
+		self.childlist_titleForException=[]
+		self.RCV=[]
 		self.output_file=open(output_filename,'w')
 
 	def startElement(self,tag,attributes):
@@ -21,16 +24,44 @@ class ClinVarHandler( xml.sax.ContentHandler):
 
 		if tag=="ClinVarSet":
 			self.Title=[]
+			self.RCV=[]
+			self.childlist_titleForException=[]
 			self.ClinaVarsetID=attributes.get('ID')
+
+		if tag=="ClinVarAccession" and attributes.get("Type")=="RCV":
+
+			if len(self.RCV)==0:
+
+				self.RCV.append(attributes.get('Acc'))
+
+			else:
+				raise xml.sax.SAXException("Ooopss more than one RCV!!")
+
+			
+
+			
+
+			
+
+
+
+
+	
+
 	def endElement (self,tag):
 
 		if self.CurrentData=="Title":
 
 			self.childlist_title = ''.join(map(str,self.Title))
+			if len(self.childlist_titleForException)==0:
+				self.childlist_titleForException.append(self.childlist_title)
+			else:
+				raise xml.sax.SAXException("Ooopss more than one Titles!!")
 
 		if tag=="ClinVarSet":
-			print("title",self.childlist_title)
-			self.data_line.append({"ID clinVar":self.ClinaVarsetID,"Title":self.childlist_title})
+			print("Accession(clinVar):",self.RCV)
+			
+			self.data_line.append({"ID clinVar":self.ClinaVarsetID,"Title":self.childlist_titleForException,"Accession(clinVar)":self.RCV})
 
 		self.CurrentData=""
 
@@ -42,8 +73,6 @@ class ClinVarHandler( xml.sax.ContentHandler):
 		if self.CurrentData=="Title":
 
 			self.Title.append(saxutils.unescape(content))
-		
-	
 
 
 
@@ -58,7 +87,3 @@ if( __name__ =="__main__"):
 	f=gzip.open("/home/maryastr/clinVar/clinVarCode/ClinVarFullRelease_2018-07.xml.gz")
 	parser.parse(f)
 	f.close()
-	
-	
-	
-	
