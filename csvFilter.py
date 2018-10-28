7367e0311596a3e64d99d9db323fd45d9281e8db
maryastrinaki <32954315+maryastrinaki@users.noreply.github.com>
2018-10-28 10:36:30 +0200

    Update csvFilter.py

--- csvFilter.py
+++ csvFilter.py
@@ -1,8 +1,10 @@
 import json
-file = open('test.json')
+#file = open('skata.json')
+file = open('euterpi.json')
 data = json.load(file) # now the JSON object is represented as Python dict
 output_file=open("output.csv",'w')
 pos=[]
+
 data_line=""
 header=["assembly","chr","start","stop","strand","clinical significance","condition(s)","GMAF","Allele frequency"]
 header_line=','.join(header)
@@ -15,35 +17,37 @@ output_file.write('\n')
 for d in data:
 	try:
 
+
 		if "single nucleotide variant" in d['variant type']:
 
 			for z,assembly in enumerate(d["Assembly"]):
 
 				if assembly=="GRCh37":
-					pos.append(z)
-					for f,m in enumerate(pos):
-
-					
-						dataLine=d['Assembly'][m],d['chr'][m],d['start'][m],d['stop'][m],d['strand'][m],d['clinical significance'],d['condition(s)'],d['Global Allele Freguency '],d['GO_ESP']
-						data_line=','.join(map(str, dataLine))
-		if d['start'][m]!=d['stop'][m]:
-			raise ValueError("That is not a single nucleotide variant!")
-		output_file.write(data_line+'\n')
+					pos=z
+					print(d['Accession'])
+					print(d['Assembly'][pos])
+					print(d['chr'][pos])
+					print(d['start'][pos])
+					print(d['stop'][pos])
+					print(d['strand'][pos])
+					print(d['clinical significance'])
+					print(d['condition(s)'])
+					print(d['Global Allele Freguency '])
+					print(d["Frequencies"],d["Accession"])
+					if d['start'][pos]!=d['stop'][pos]:
+						raise ValueError("That is not a single nucleotide variant!")
+					dataLine=d['Assembly'][pos],d['chr'][pos],d['start'][pos],d['stop'][pos],d['strand'][pos],d['clinical significance'],d['condition(s)'],d['Global Allele Freguency '],d['Frequencies']
+					data_line=','.join(map(str, dataLine))
+					output_file.write(data_line+'\n')
 
 	except ValueError as ve:
 		print(ve)
-	
-print("finished")
-						
 
 
 
-	
 
 
-	
-				
-
-		
+	#except IndexError:
+	#	pass
 
 output_file.close() 
